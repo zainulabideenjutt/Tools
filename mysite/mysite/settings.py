aa2092b1fdd0e2563c896ddfaae84c54d17baa20
zain ul abideen <zainulabideen.jutt1@gmail.com>
2022-12-05 23:51:02 +0500

    Setted Up Debug=False

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i2340nz_^3o+tyvczmgze6=+c9978%&nhv-$*v#9&cn%xf++t^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
   'localhost',
@@ -122,7 +122,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = 'static'
 # Media FIles
 MEDIA_URL = '/media/'
